5692106e1a1bce73ecec0af48cda309038d3b013
hrayr <hrayrgrigoryan93@gmail.com>
2021-08-05 22:38:15 +0400

    first commit

---
+++ Js/JsMy.js
@@ -0,0 +1,26 @@
+"use strict";
+/*
+//переменные
+
+let number = 5;
+let str = "string";
+let bool = true/false;
+let null,undefined,NAN,symbol;
+const leftBorderWidth =1;
+*/
+
+/*
+// alert,prompt.confirm
+const result = confirm("вопрос?");
+alert ("text");
+const answer = prompt("вопрос","default ");
+console.log(typeOf(answer));*/
+
+let age = 25;
+console.log (`my age ${age}`);
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
