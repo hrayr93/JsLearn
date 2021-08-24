fdcf75ea3484e864b083da1cb2e43a7892e1080a
hrayr <hrayrgrigoryan93@gmail.com>
2021-08-24 19:42:55 +0400

    learn 1

--- Js/JsMy.js
+++ Js/JsMy.js
@@ -16,11 +16,20 @@ alert ("text");
 const answer = prompt("вопрос","default ");
 console.log(typeOf(answer));*/
 
-let age = 25;
-console.log (`my age ${age}`);
-let incr = 10,
-    decr = 10;
-incr++;
-decr--;
-console.log(incr);
-console.log(decr);
+const numberOfFilms= +prompt("Сколько фильмов вы уже посмотрелиЬ","");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+const a = prompt("Один из последних просморенныж фильмов?",""),
+    b = prompt("На сколько оцените его?",""),
+    c = prompt("Один из последних просморенныж фильмов?",""),
+    d = prompt("На сколько оцените его?","");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
