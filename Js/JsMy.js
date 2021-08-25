c2708a3b0931120376b19bb7d44348dfdc493877
hrayr <hrayrgrigoryan93@gmail.com>
2021-08-25 14:41:06 +0400

    while

--- Js/JsMy.js
+++ Js/JsMy.js
@@ -16,7 +16,7 @@ alert ("text");
 const answer = prompt("вопрос","default ");
 console.log(typeOf(answer));*/
 
-const numberOfFilms= +prompt("Сколько фильмов вы уже посмотрели","");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -25,25 +25,43 @@ const personalMovieDB = {
     privat: false
 
 };
-    for(let i=0;i<2;i++){
-        const a = prompt("Один из последних просморенных фильмов?",""),
-            b = prompt("На сколько оцените его?","");
-            if (a != null && b != null  && a != "" && b !="" && a.length <50 ){
-                personalMovieDB.movies[a] = b;
-                console.log("done");
-            }else{
-                console.log("error");
-                i--;
-            }
+
+//loop for (){}
+/* for(let i=0;i<2;i++){
+     const a = prompt("Один из последних просморенных фильмов?",""),
+         b = prompt("На сколько оцените его?","");
+         if (a != null && b != null  && a != "" && b !="" && a.length <50 ){
+             personalMovieDB.movies[a] = b;
+             console.log("done");
+         }else{
+             console.log("error");
+             i--;
+         }
+
+}*/
+//loop while
+let i = 0;
+while (i < 2) {
+    const a = prompt("Один из последних просморенных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    } else {
+        console.log("error");
+        i--;
+    }
+    i++
 
 }
-    if (personalMovieDB.count < 10 ){
-        console.log(1);
-    }else if (personalMovieDB >= 10 && personalMovieDB < 30){
-        console.log(2);
-    }else if (personalMovieDB >= 30){
-        console.log(3);
-    }else {
-        console.log("Error");
+
+if (personalMovieDB.count < 10) {
+    console.log(1);
+} else if (personalMovieDB >= 10 && personalMovieDB < 30) {
+    console.log(2);
+} else if (personalMovieDB >= 30) {
+    console.log(3);
+} else {
+    console.log("Error");
 }
 console.log(personalMovieDB);
